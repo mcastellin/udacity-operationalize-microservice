5092e01b2185d38e25005c2637e3505ea8f475f3
Manuel Castellin <manuel@castellinconsulting.com>
2020-03-16 19:33:30 +0000

    chore: containerize the make-prediction application
    The provided application has been containerized.
    - docker container build with applicatino files
    - `app.py` update to include additional logging information
    - correction to `Makefile` to exclude linting warning for python
    formatted strings
    - scripts to create, run and upload docker image to docker hub
    - captured shell output from docker container in `docker_out.txt`

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
+DOCKER_TAG=make-prediction:latest
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=${DOCKER_TAG} --no-cache .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 ${DOCKER_TAG}
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerimage=make-prediction:latest
+dockerpath=mcastellin/udacity-make-prediction
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag $dockerimage $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
