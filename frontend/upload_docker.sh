3a9f64ff417cdf2486b07e7b78520bb1c7896784
Manuel Castellin <manuel@castellinconsulting.com>
2020-03-17 15:16:09 +0000

    feat(frontend): add docker build for frontend app
    Building frontend application docker container
    - provided docker build and upload scripts
    - updated makefile to include linting for new dockerfile

---
+++ frontend/upload_docker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerimage=make-prediction-frontend:latest
+dockerpath=mcastellin/udacity-make-prediction-frontend
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login --username $DOCKER_USER -p $DOCKER_PASSWORD
+docker tag $dockerimage $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
